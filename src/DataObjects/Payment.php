b81417cd54a12c8e313e3ffe4f7528f9bcb9671a
mortenebak <mortenebak@users.noreply.github.com>
2024-01-07 18:58:13 +0000

    Fix styling

--- src/DataObjects/Payment.php
+++ src/DataObjects/Payment.php
@@ -5,12 +5,11 @@ namespace Netbums\Quickpay\DataObjects;
 readonly class Payment
 {
     public function __construct(
-        public string           $currency,
-        public string           $order_id,
-        public Basket           $basket,
+        public string $currency,
+        public string $order_id,
+        public Basket $basket,
         public ?OptionalAddress $invoice_address,
         public ?OptionalAddress $shipping_address,
-    )
-    {
+    ) {
     }
 }
